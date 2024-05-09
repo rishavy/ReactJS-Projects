883c02742c9eab5c43b9907e1fa98651c87d7726
RISHAV YADAV <128891361+rishavy@users.noreply.github.com>
2024-05-09 16:37:36 +0000

    update ui

--- Password generator/src/assets/components/PasswordGenerator.jsx
+++ Password generator/src/assets/components/PasswordGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiCopy } from 'react-icons/fi';
 
 const PasswordGenerator = () => {
@@ -9,6 +9,7 @@ const PasswordGenerator = () => {
     const [includeNumbers, setIncludeNumbers] = useState(true);
     const [includeSpecialChars, setIncludeSpecialChars] = useState(true);
     const [copied, setCopied] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const generatePassword = () => {
         const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -22,13 +23,24 @@ const PasswordGenerator = () => {
         if(includeNumbers) chars += numberChars;
         if(includeSpecialChars) chars += specialChars;
 
+        if (chars.length === 0) {
+            setErrorMessage('* Please select at least one option for password generation');
+            setPassword('');
+            return;
+        }
+
         let generatedPassword = '';
         for(let i = 0; i < passwordLength; i++) {
             const randomIndex = Math.floor(Math.random() * chars.length);
             generatedPassword += chars[randomIndex];
         }
         setPassword(generatedPassword);
+        setErrorMessage('');
     };
+
+    useEffect(() => {
+        generatePassword();
+    }, [includeUppercase, includeLowercase, includeNumbers, includeSpecialChars, passwordLength]);
 
     const handleCopy = () => {
         navigator.clipboard.writeText(password);
@@ -37,21 +49,28 @@ const PasswordGenerator = () => {
     };
 
     return (
-        <div className="max-w-md mx-auto mt-8 p-4 bg-[#f7f7f7] rounded-lg shadow-lg backdrop-blur-md">
-        <h1 className="text-3xl font-bold mb-4">Password Generator</h1>
-        <div className="mb-4">
+        <div className="max-w-xl mx-auto mt-8 p-4 bg-[#ffffffe7] rounded-lg shadow-lg backdrop-blur-md">
+        <h1 className="text-3xl font-bold mb-6">Password Generator</h1>
+        <div className="mt-4 flex items-center">
+            <input type="text" value={password} readOnly className="w-full p-2 border rounded-md mr-2" />
+            <button onClick={handleCopy} className="bg-[#4299e1] text-white font-semibold px-4 py-2 rounded hover:bg-[#3182ce]">
+            {copied ? 'Copied!' : <FiCopy />}
+            </button>
+        </div>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <div className="mb-4 mt-4">
             <label className="block mb-2">Password Length: {passwordLength}</label>
             <input
                 type="range"
-                min="1"
+                min="4"
                 max="50"
                 value={passwordLength}
                 onChange={(e) => setPasswordLength(parseInt(e.target.value))}
                 className="w-full"
             />
         </div>
-        <div className="mb-4 flex items-center">
-            <label className="block mb-2 mr-4">
+        <div className="mb-1 flex items-center">
+            <label className="block mb-1 mr-4">
             <input
                 type="checkbox"
                 checked={includeUppercase}
@@ -61,8 +80,8 @@ const PasswordGenerator = () => {
             &nbsp; Capital Letters (A-Z)
             </label>
         </div>
-        <div className="mb-4 flex items-center">
-            <label className="block mb-2 mr-4">
+        <div className="mb-1 flex items-center">
+            <label className="block mb-1 mr-4">
                 <input
                     type="checkbox"
                     checked={includeLowercase}
@@ -72,8 +91,8 @@ const PasswordGenerator = () => {
                 &nbsp; Small Letters (a-z)
             </label>
         </div>
-        <div className="mb-4 flex items-center">
-            <label className="block mb-2 mr-4">
+        <div className="mb-1 flex items-center">
+            <label className="block mb-1 mr-4">
             <input
                 type="checkbox"
                 checked={includeNumbers}
@@ -83,8 +102,8 @@ const PasswordGenerator = () => {
             &nbsp; Numbers (0-9)
             </label>
         </div>
-        <div className="mb-4 flex items-center">
-            <label className="block mb-2 mr-4">
+        <div className="mb-1 flex items-center">
+            <label className="block mb-1 mr-4">
             <input
                 type="checkbox"
                 checked={includeSpecialChars}
@@ -94,15 +113,9 @@ const PasswordGenerator = () => {
             &nbsp; Special Characters (!@#$%...)
             </label>
         </div>
-        <button onClick={generatePassword} className="bg-[#4299e1] text-white px-4 py-2 rounded hover:bg-[#3182ce] *:-600">
+        <button onClick={generatePassword} className="mt-4 bg-[#4299e1] text-white font-semibold tracking-wide px-28 py-2 rounded hover:bg-[#3182ce]">
         Generate Password
         </button>
-        <div className="mt-4 flex items-center">
-            <input type="text" value={password} readOnly className="w-full p-2 border rounded-md mr-2" />
-            <button onClick={handleCopy} className="bg-[#4299e1] text-white px-4 py-2 rounded hover:bg-[#3182ce]">
-            {copied ? 'Copied!' : <FiCopy />}
-            </button>
-        </div>
         </div>
     );
 };
